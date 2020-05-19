a05e5a41fa04ea1dce39356715ad6d65457e8296
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2020-05-19 10:36:48 +1000

    Destroy Objects (Crates) Working

---
+++ Assets/Scripts/Obstacle/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public float curHealth;
+    public float maxHealth;
+
+    public GameObject healthCanvas;
+    public Image healthBar;
+    void Start()
+    {
+        maxHealth = curHealth;
+    }
+
+    void LateUpdate()
+    {
+        if (healthBar.fillAmount < 1 && healthBar.fillAmount > 0)
+        {
+            healthCanvas.SetActive(true);
+            healthCanvas.transform.LookAt(Camera.main.transform);
+            healthCanvas.transform.Rotate(0, 180, 0);
+        }
+        else if (healthCanvas.activeSelf == true)
+        {
+            healthCanvas.SetActive(false);
+        }
+    }
+
+    public void Damage(float amount)
+    {
+        curHealth -= amount;
+        healthBar.fillAmount = curHealth;
+        if (curHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
