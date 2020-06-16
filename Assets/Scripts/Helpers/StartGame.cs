6edb7db094a8ef736ac95fa0402b5d4fd0efc6fe
Undeddy-Gamer <54134838+Undeddy-Gamer@users.noreply.github.com>
2020-06-16 11:06:07 +1000

    wait screen, finished

---
+++ Assets/Scripts/Helpers/StartGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    private float seconds = 4;
+    [SerializeField]
+    Text startCount;
+    [SerializeField]
+    GameObject startScreen;
+
+    void Start()
+    {
+        Time.timeScale = 0;
+        
+            StartCoroutine(Countdown());
+        
+    }
+
+
+    IEnumerator Countdown()
+    {       
+        if (--seconds == 0)
+        {
+            CancelInvoke("Countdown");
+            startScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            startCount.text = "Starting : " + seconds;
+        }
+        yield return new WaitForSecondsRealtime(1);
+        StartCoroutine(Countdown());
+    }
+}
